3b36501adba0b77ee7f34713dca4251b4cc3968e
PedroHS05 <pedro.scabelo2b2@gmail.com>
2025-05-12 12:47:05 -0300

    Aula 12/05

---
+++ HelpApp/HelpApp.Application/Interfaces/ICategoryServices.cs
@@ -0,0 +1,15 @@
+﻿using HelpApp.Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HelpApp.Application.Interfaces
+{
+    public interface ICategoryServices
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategories();
+        Task<CategoryDTO> GetById(int? id);
+        Task Add(CategoryDTO categoryDto);
+        Task Update(CategoryDTO categoryDto);
+        Task Remove(int? id);
+    }
+}
